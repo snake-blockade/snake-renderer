6169e3dd7392ac12c0e7d102e952c13e9b15cfc6
Yanis Tahiri <yanistahiri560@gmail.com>
2024-05-31 02:13:05 +0200

    feat: ✨add render for the different body parts, head rotation & tail rotation.

--- src/main/java/io/zentae/renderer/layer/DefaultTranslationLayer.java
+++ src/main/java/io/zentae/renderer/layer/DefaultTranslationLayer.java
@@ -4,6 +4,7 @@ import io.zentae.snake.engine.entity.ArenaEntity;
 import io.zentae.snake.engine.entity.Location;
 import io.zentae.snake.engine.entity.arena.Arena;
 import io.zentae.snake.engine.entity.snake.Snake;
+import io.zentae.snake.engine.movement.Movement;
 import jakarta.annotation.Nonnull;
 
 import java.util.HashMap;
@@ -11,55 +12,140 @@ import java.util.Map;
 
 public class DefaultTranslationLayer extends TranslationLayer {
 
-    public DefaultTranslationLayer(int tileSize, @Nonnull Map<Class<?>, Character> characterMap) {
-        super(tileSize, characterMap);
+    public DefaultTranslationLayer(int tileSize, @Nonnull Map<Class<?>, String> stringMap) {
+        super(tileSize, stringMap);
     }
 
     // TODO FAIRE LES VIRAGES DU SERPENT
     @Nonnull
     @Override
-    public Map<Location, Character> translate(Arena arena) {
+    public Map<Location, String> translate(Arena arena) {
         // prepare our render.
-        Map<Location, Character> render = new HashMap<>();
+        Map<Location, String> render = new HashMap<>();
         // do the borders.
-        for(int y = 0; y < arena.getWidth(); y++) {
-            for(int x = 0; x < arena.getLength(); x++)
-                if(x + 1 == arena.getLength()
+        for (int y = 0; y < arena.getWidth(); y++) {
+            for (int x = 0; x < arena.getLength(); x++)
+                if (x + 1 == arena.getLength()
                         || x == 0
                         || y == 0
                         || y + 1 == arena.getWidth()) {
-                    render.put(new Location(x * getTileSize(), y * getTileSize()), 'X');
+                    render.put(new Location(x * getTileSize(), y * getTileSize()), "X");
                 } else {
-                    render.put(new Location(x * getTileSize(), y * getTileSize()), ' ');
+                    render.put(new Location(x * getTileSize(), y * getTileSize()), " ");
                 }
 
         }
         // loop through each object.
-        for(ArenaEntity entity : arena.getObjects()) {
-            if(entity instanceof Snake)
+        for (ArenaEntity entity : arena.getObjects()) {
+            if (entity instanceof Snake)
                 continue;
             // try to retrieve the character associated.
-            Character character = getCharacterMap().get(entity.getType());
+            String string = getCharacterMap().get(entity.getType());
             // check if there's character mapped.
-            if(character == null)
+            if (string == null)
                 continue;
             // loop through each segment.
-            for(Location segment : entity.getSegments()) {
+            for (Location segment : entity.getSegments()) {
                 render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
-                        character);
+                        string);
             }
         }
-        for(Snake snake : arena.getObjects(Snake.class)) {
-            // try to retrieve the character associated.
-            Character character = getCharacterMap().get(Snake.class);
+        for (Snake snake : arena.getObjects(Snake.class)) {
             // loop through each segment.
-            for(Location segment : snake.getBody()) {
-                if(snake.getHead().equals(segment)) {
+            for (int i = 0; i < snake.getBody().size(); i++) {
+                // retrieve the current segment.
+                Location segment = snake.getBody().get(i);
+                // check if we're dealing with the head.
+                if (i == 0) {
+                    // calculate the x offset.
+                    int xOffset = snake.getHead().getX() - snake.getBody().get(1).getX();
+                    // calculate the y offset.
+                    int yOffset = snake.getHead().getY() - snake.getBody().get(1).getY();
+                    // retrieve the movement associated.
+                    // the movement cannot be null.
+                    Movement movement = Movement.byOrientation(xOffset, yOffset);
+                    // check if the movement is null.
+                    if(movement == null)
+                        continue;
+                    // check if the body is under.
                     render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
-                            'H');
+                            "head_" + movement.toString().toLowerCase());
+                } else if (i == snake.getBody().size() - 1) {
+                    // calculate the x offset.
+                    int xOffset = snake.getBody().get(snake.getBody().size() - 2).getX() - snake.getTail().getX();
+                    // calculate the y offset.
+                    int yOffset = snake.getBody().get(snake.getBody().size() - 2).getY() - snake.getTail().getY();
+                    // retrieve the movement associated.
+                    // the movement cannot be null.
+                    Movement movement = Movement.byOrientation(xOffset, yOffset);
+                    // check if the movement is null.
+                    if(movement == null)
+                        continue;
+                    // check if the body is under.
+                    render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                            "tail_" + movement.toString().toLowerCase());
                 } else {
-                    render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
-                            character);
+                    // retrieve the body part before and after the current one.
+                    Location before = snake.getBody().get(i - 1);
+                    Location after = snake.getBody().get(i + 1);
+                    // check if we're on a vertical case.
+                    if (before.getX() == segment.getX() && after.getX() == segment.getX()) {
+                        render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                "body_vertical");
+                        continue;
+                    }
+                    if (before.getY() == segment.getY() && after.getY() == segment.getY()) {
+                        render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                "body_horizontal");
+                        continue;
+                    }
+                    if(after.getY() > segment.getY()) {
+                        if (before.getX() > segment.getX()) {
+                            render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                    "body_top_right");
+                            continue;
+                        }
+                        if (before.getX() < segment.getX()) {
+                            render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                    "body_top_left");
+                            continue;
+                        }
+                    }
+                    if(after.getY() < segment.getY()) {
+                        if(before.getX() > segment.getX()) {
+                            render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                    "body_bottom_right");
+                            continue;
+                        }
+                        if(before.getX() < segment.getX()) {
+                            render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                    "body_bottom_left");
+                            continue;
+                        }
+                    }
+                    if(after.getY() == segment.getY()) {
+                        if(before.getY() < segment.getY()) {
+                            if(after.getX() > segment.getX()) {
+                                render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                        "body_bottom_right");
+                                continue;
+                            }
+                            if(after.getX() < segment.getX()) {
+                                render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                        "body_bottom_left");
+                                continue;
+                            }
+                        }
+                        if(before.getY() > segment.getY()) {
+                            if(after.getX() > segment.getX()) {
+                                render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                        "body_top_right");
+                                continue;
+                            }
+                            render.put(new Location(segment.getX() * getTileSize(), segment.getY() * getTileSize()),
+                                    "body_top_left");
+                        }
+                    }
                 }
             }
         }
